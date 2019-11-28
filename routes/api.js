c4ba5217c4a9ace18f7b62ccb0a4d0bde1b9c3b3
Bora Manga <bora.mng525@gmail.com>
2019-11-28 14:27:36 +0100

    TASK 3 completed

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 //shortid
-var { getRecords, pushRecord } = require("../userController");
+var {
+  getRecords,
+  pushRecord,
+  getRecordsId,
+  deleteRecordsId,
+  putRecord
+} = require("../userController");
 
 /* GET users listing. */
 router.get("/records", getRecords);
 /* POST users listing. */
 router.post("/records", pushRecord);
+router.get("/records/:id", getRecordsId);
+router.delete("/records/:id", deleteRecordsId);
+router.put("/records/:id", putRecord);
 module.exports = router;
