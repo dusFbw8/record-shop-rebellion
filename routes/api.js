fedd414f7f695c33aadbfe784b032b0f4ae90080
Bora Manga <bora.mng525@gmail.com>
2019-11-28 15:23:53 +0100

    used req.body to take the info

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 //shortid
 var {
   getRecords,
-  pushRecord,
+  postRecord,
   getRecordsId,
   deleteRecordsId,
   putRecord
@@ -12,7 +12,7 @@ var {
 /* GET users listing. */
 router.get("/records", getRecords);
 /* POST users listing. */
-router.post("/records", pushRecord);
+router.post("/records", postRecord);
 router.get("/records/:id", getRecordsId);
 router.delete("/records/:id", deleteRecordsId);
 router.put("/records/:id", putRecord);
