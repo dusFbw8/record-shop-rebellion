355ee643b3163aa2a14e1179c3804cb8c2173fec
Bora Manga <bora.mng525@gmail.com>
2019-12-19 12:29:48 +0100

    Mongoose Models added

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const db = low(adapter);
 //shortid
 var shortid = require("shortid");
 /** ROUTERS */
-const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
 
 /** INIT */
@@ -37,8 +36,7 @@ app.use(cors);
 app.use(express.static(path.join(__dirname, "public")));
 
 /** ROUTES */
-app.use("/", indexRouter);
-app.use("/api", apiRouter);
+app.use("/", apiRouter);
 //ERROR
 
 app.use((req, res, next) => {
